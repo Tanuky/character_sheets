8d65d13b058919f909ad82363eba97b52c1a1b45
Antoine Frere <antoine.frere@capgemini.com>
2019-01-09 11:26:47 +0100

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DdFiveSheetComponent } from './dd-five-sheet/dd-five-sheet.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DdFiveSheetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dd-five-sheet/dd-five-sheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dd-five-sheet',
+  templateUrl: './dd-five-sheet.component.html',
+  styleUrls: ['./dd-five-sheet.component.css']
+})
+export class DdFiveSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
